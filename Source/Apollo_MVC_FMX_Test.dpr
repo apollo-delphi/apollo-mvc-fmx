a03ae3a6138fe598dd20ccee2293850c32adc6f9
alezzle <niekras@tut.by>
2021-02-19 11:32:08 +0300

    dev

--- Source/Apollo_MVC_FMX_Test.dpr
+++ Source/Apollo_MVC_FMX_Test.dpr
@@ -8,13 +8,12 @@ uses
   DUnitX.Loggers.Xml.NUnit,
   DUnitX.TestFramework,
   tstApollo_MVC_FMX in 'tstApollo_MVC_FMX.pas',
-  Apollo_MVC_FMX in 'Apollo_MVC_FMX.pas' {Form1},
+  Apollo_MVC_FMX in 'Apollo_MVC_FMX.pas' {ViewFMXMain},
   Apollo_MVC_Core in '..\Vendors\Apollo_MVC_Core\Apollo_MVC_Core.pas';
 
 begin
   Application.Initialize;
   Application.Title := 'DUnitX';
   Application.CreateForm(TGUIVCLTestRunner, GUIVCLTestRunner);
-  Application.CreateForm(TForm1, Form1);
   Application.Run;
 end.
